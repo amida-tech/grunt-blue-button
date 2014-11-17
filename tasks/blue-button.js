5c6cb0e57050b6086ee89ed0354c20b4556be04b
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2014-11-17 07:48:33 -0500

    handle cms, version increment

--- tasks/blue-button.js
+++ tasks/blue-button.js
@@ -3,6 +3,8 @@
 var bb = require('blue-button');
 var path = require('path');
 
+
+
 module.exports = function(grunt) {
 	grunt.registerMultiTask('blue-button', "Parse CCDA files.", function() {
 		this.files.forEach(function(filePair) {
@@ -10,9 +12,11 @@ module.exports = function(grunt) {
 
 			filePair.src.forEach(function(src) {
 				var content = grunt.file.read(src);
-        		var result = bb.parseString(content);
+				var sensed = bb.senseString(content);
+				var cms = sensed.type === 'cms';
+        		var result = cms ? bb.parseText(content) : bb.parseString(content);
 
-        		var baseName = path.basename(src, '.xml');
+        		var baseName = path.basename(src, path.extname(src));
         		var destName = baseName + '.json';
         		var destPath = path.join(dest, destName);
         		var destContent = JSON.stringify(result, undefined, 2);
