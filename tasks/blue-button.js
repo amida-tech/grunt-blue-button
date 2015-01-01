b16f858a8764a65fda4ad3a8981a4f3fcc863ba2
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-01-01 18:57:11 -0500

    add blue-button-generate task

--- tasks/blue-button.js
+++ tasks/blue-button.js
@@ -2,13 +2,15 @@ var path = require('path');
 
 var bb = require('blue-button');
 var bbm = require('blue-button-model');
+var bbg = require('blue-button-generate');
 
 module.exports = function(grunt) {
 	"use strict";
 
 	grunt.registerMultiTask('blue-button', "Parse CCDA, C32, or CMS files.", function() {
 		var options = this.options({
-			validate: false
+			validate: false,
+			postfix: ""
 		});
 		this.files.forEach(function(filePair) {
 			var dest = filePair.dest;
@@ -24,10 +26,29 @@ module.exports = function(grunt) {
         			}
         		}
         		var baseName = path.basename(src, path.extname(src));
-        		var destName = baseName + '.json';
+        		var destName = baseName + options.postfix + '.json';
         		var destPath = path.join(dest, destName);
         		var destContent = JSON.stringify(result, undefined, 2);
         		grunt.file.write(destPath, destContent);
+			});
+		});
+	});
+
+	grunt.registerMultiTask("blue-button-generate", "Generate CCDA from BB JSON.", function() {
+		var options = this.options({
+			postfix: ""
+		});
+		this.files.forEach(function(filePair) {
+			var dest = filePair.dest;
+
+			filePair.src.forEach(function(src) {
+				var content = grunt.file.read(src);
+        		var json = JSON.parse(content);
+        		var xml = bbg.generateCCD(json);
+        		var baseName = path.basename(src, path.extname(src));
+        		var destName = baseName + options.postfix + '.xml';
+        		var destPath = path.join(dest, destName);
+        		grunt.file.write(destPath, xml);
 			});
 		});
 	});
